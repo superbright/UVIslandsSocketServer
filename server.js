ea1b546ddf5f55ede7a136238018edd3487b25eb
M Ahmed <mozgen.dev@gmail.com>
2020-09-25 02:37:19 +0500

    updated configs

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var app = require('express')();
 var http = require('http').createServer(app);
 var io = require('socket.io')(http);
-const PORT = 9090;
+const PORT = 3000;
 io.on('connection', (socket) => {
   console.log('a user connected');
 
